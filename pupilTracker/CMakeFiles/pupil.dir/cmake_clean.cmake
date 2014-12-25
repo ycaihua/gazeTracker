0f254680b6971e1babe6a258de3bdcc14e4b04a0
Manasi94 <manasimuglikar@gmail.com>
2014-12-25 19:21:21 +0530

    Pupil tracking using haar

---
+++ pupilTracker/CMakeFiles/pupil.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pupil.dir/pupil.cpp.o"
+  "pupil.pdb"
+  "pupil"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pupil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
